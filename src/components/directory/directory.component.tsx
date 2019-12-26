a9a3cb0ad1dab20f11931a52df52bae398cf20ef
Pablo Gracia <pablo.graciag@gmail.com>
2019-12-26 10:23:44 +0100

    Broken homepage into smaller components, finished styling menu item component

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import "./directory.styles.scss";
+
+import { MenuItem } from "../menu-item/menu-item.component";
+
+interface IProps {}
+interface IState {
+  sections: IItem[];
+}
+
+export interface IItem {
+  title: string;
+  imageUrl?: string;
+  size?: string;
+  id?: number;
+}
+
+export class Directory extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      sections: [
+        {
+          title: "HATS",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2
+        },
+        {
+          title: "SNEAKERS",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3
+        },
+        {
+          title: "WOMEN",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4
+        },
+        {
+          title: "MEN",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, size, id }) => (
+          <MenuItem key={id} title={title} size={size} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
