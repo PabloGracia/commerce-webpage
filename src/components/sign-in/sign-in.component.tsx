08917bf24d91581bce0e422afa76d4152cfe57a9
Pablo Gracia <pablo.graciag@gmail.com>
2020-01-20 18:17:16 +0100

    Revert "finishing sign in handleSubmit method"
    This reverts commit c216a39052cb6973e1843c72b9854c4e8190640c.

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 import { FormInput } from "../form-input/form-input.component";
 import { CustomButton } from "../custom-button/custom-button.component";
 
-import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 interface IProps {}
 interface IState {
@@ -30,20 +30,10 @@ export class SignIn extends React.Component<IProps, IState> {
     };
   }
 
-  handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: "", password: "" });
-    } catch (error) {
-      if (error.code === "auth/wrong-password") {
-        alert("The password is invalid or the user does not have a password");
-      }
-      console.log(error);
-    }
+    this.setState({ email: "", password: "" });
   };
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
