241bfecb2aa02485f7991929cda856d08e9dce14
Pablo Gracia <pablo.graciag@gmail.com>
2020-02-07 07:23:08 +0100

    Fixed overflowing div

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -16,14 +16,15 @@ const CartDropdown: React.FC<Pick<CartStateType, "cartItems"> &
   RouteComponentProps &
   PropsFromRedux> = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
-    {cartItems.length ? (
-      cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
-    ) : (
-      <span className="empty-message">Your cart is empty</span>
-    )}
+    <div className="cart-items">
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
+    </div>
     <CustomButton
       onClick={() => {
         history.push("/checkout");
